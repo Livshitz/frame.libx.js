3c1899b1f5056081c03ef6fddf776b80068771b8
Elya Livshitz <elya.livshitz@gmail.com>
2022-09-24 17:54:20 +0300

    * fix build by fixing `fix-buefy.sh` to choose right command per Mac or unix

--- fix-buefy.sh
+++ fix-buefy.sh
@@ -1,1 +1,5 @@
-sed -i '' 's/\"..\/..\/node_modules/\".\/node_modules/g' ./node_modules/buefy/src/scss/buefy-build.scss
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed -i '' 's/\"..\/..\/node_modules/\".\/node_modules/g' ./node_modules/buefy/src/scss/buefy-build.scss
+else
+	sed -i 's/\"..\/..\/node_modules/\".\/node_modules/g' ./node_modules/buefy/src/scss/buefy-build.scss
+fi
