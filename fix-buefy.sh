1664fbde84af3ca1bde97dc149fc484a7ae7540c
Elya Livshitz <elya.livshitz@gmail.com>
2022-09-24 17:57:22 +0300

    * fix-buefy: better syntax for DRY

--- fix-buefy.sh
+++ fix-buefy.sh
@@ -1,5 +1,6 @@
+SEDOPTION=
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' 's/\"..\/..\/node_modules/\".\/node_modules/g' ./node_modules/buefy/src/scss/buefy-build.scss
-else
-	sed -i 's/\"..\/..\/node_modules/\".\/node_modules/g' ./node_modules/buefy/src/scss/buefy-build.scss
+  SEDOPTION="-i ''"
 fi
+
+sed $SEDOPTION 's/\"..\/..\/node_modules/\".\/node_modules/g' ./node_modules/buefy/src/scss/buefy-build.scss
